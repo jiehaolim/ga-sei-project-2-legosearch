091faf2b47da2222d40680f5cf11f123f20fc9c6
limjiehao <jiehao15@gmail.com>
2022-09-01 19:53:00 +0800

    16th commit - updated dropdownlist

--- src/components-layout/DropDownList.jsx
+++ src/components-layout/DropDownList.jsx
@@ -1,24 +1,23 @@
 import { useEffect, useState } from "react"
-
-const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY
 
 const DropDownList = () => {
-  const [themes, setThemes] = useState([]);
+  const [themes, setThemes] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
-    // max data pull is 1000 but lego only have 446 themes as of 31 August 2022
+    // max data pull is 1000 but lego only have 446 themes as now 1-Sep-22
     const pageSize = 1000;
-      const response = await fetch(
-        `https://rebrickable.com/api/v3/lego/themes/?key=${API_KEY}&page_size=${pageSize}`
-      );
-      const data = await response.json();
-      // only show the main themes, filter away sub themes with parent_id null
-      const mainThemes = data.results.filter((element) => element.parent_id === null).sort((a, b) => (a.name > b.name) ? 1 : -1)
-      setThemes(mainThemes);
-    };
-    fetchData();
-  }, []);
+    const response = await fetch(
+      `https://rebrickable.com/api/v3/lego/themes/?key=${API_KEY}&page_size=${pageSize}`
+    )
+    const data = await response.json();
+    // only show the main themes, filter away sub themes with parent_id null
+    const mainThemes = data.results.filter((element) => element.parent_id === null).sort((a, b) => (a.name > b.name) ? 1 : -1)
+    setThemes(mainThemes)
+    }
+    fetchData()
+  }, [])
   
   return (
     <select name="themes" id="themes">
@@ -28,4 +27,4 @@ const DropDownList = () => {
   );
 };
 
-export default DropDownList;
+export default DropDownList
