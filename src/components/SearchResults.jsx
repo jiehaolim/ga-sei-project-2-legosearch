63b38c36235a4ff7f880d590d294fcabb9e7f0fb
limjiehao <jiehao15@gmail.com>
2022-10-06 20:47:46 +0800

    37th commit - updated link in search results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,17 +1,30 @@
+import { Link, useLocation } from "react-router-dom";
+
 const SearchResults = ({ result }) => {
+  const location = useLocation()
+  let path = ""
+  // update the URL per location
+  if (location.pathname.startsWith("/sets")) {
+    path = "/sets/result"
+  } else if (location.pathname.startsWith("/minifigures")) {
+    path = "/minifigures/result"
+  }
+
   return (
     <>
       <div>main results</div>
       {result.results.map((element) => (
-        <div key={element.set_num}>
-          <div>{element.set_num}</div>
-          <div>{element.name}</div>
-          <div>{element.year}</div>
-          <div>{element.num_parts}</div>
-          <div>{element.set_img_url}</div>
-          <br />
-        </div>
+        <Link key={element.set_num} to={`/sets/result/${element.set_num}`}>
+              <div>{element.set_num}</div>
+              <div>{element.name}</div>
+              <div>{element.year}</div>
+              <div>{element.num_parts}</div>
+              <div>{element.set_img_url}</div>
+              <br/>
+              <br/>
+          </Link>
       ))}
+
     </>
   );
 };
