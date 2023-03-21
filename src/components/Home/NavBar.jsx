ea6d37b1e6c2e3bf6c52397941a57169e4a6a0cd
limjiehao <jiehao15@gmail.com>
2023-03-22 01:07:52 +0800

    75th commit - bug fixed for navbar

--- src/components/Home/NavBar.jsx
+++ src/components/Home/NavBar.jsx
@@ -66,19 +66,20 @@ const NavBar = () => {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pt-2 pb-3">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.to}
-                  className={
-                    item.to === location.pathname
-                      ? "bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-                      : "text-gray-300 hover:bg-yellow-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-                  }
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
+                <Link to={item.to}>
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    className={
+                      item.to === location.pathname
+                        ? "bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+                        : "text-gray-300 hover:bg-yellow-400 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+                    }
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                </Link>
               ))}
             </div>
           </Disclosure.Panel>
