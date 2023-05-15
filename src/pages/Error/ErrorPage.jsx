e703a9389e33bc3e244c6aefd6601abc418ccaaf
limjiehao <jiehao15@gmail.com>
2023-05-15 16:28:47 +0800

    127th commit - updated error page

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
 import wrongrequest from "../../img/error/wrongrequest400.jpeg";
+import unauthorised from "../../img/error/unauthorised401.jpeg";
+import forbidden from "../../img/error/forbidden403.jpeg";
 import pagenotfound from "../../img/error/pagenotfound404.jpeg";
-import sadlegoface from "../../img/error/sadlegoface429.jpg";
+import toomanyrequests from "../../img/error/toomanyrequests429.jpeg";
 
 const ErrorPage = () => {
   const { id } = useParams();
@@ -9,19 +11,27 @@ const ErrorPage = () => {
   const errorObj = {
     400: {
       img: wrongrequest,
-      h1: "Invalid search parameters",
+      h1: "Invalid search parameters.",
       p: "Please check your search parameters again.",
     },
-    401: { img: "", h1: "", p: "" },
-    403: { img: "", h1: "", p: "" },
+    401: {
+      img: unauthorised,
+      h1: "Unauthorized. Your API key is invalid.",
+      p: "Please contact the site administrator.",
+    },
+    403: {
+      img: forbidden,
+      h1: "Forbidden. You do not have access on the requested item.",
+      p: "Please contact the site administrator.",
+    },
     404: {
       img: pagenotfound,
-      h1: "Page not found",
+      h1: "Page not found.",
       p: "The page you are looking for doesn't exist.",
     },
     429: {
-      img: sadlegoface,
-      h1: "Too many requests received",
+      img: toomanyrequests,
+      h1: "Too many requests received.",
       p: "We're sorry, but you have sent too many requests to us recently. Please try again later",
     },
   };
@@ -41,7 +51,7 @@ const ErrorPage = () => {
       </div>
       <div className="text-center">
         <p className="mt-4 text-3xl font-bold text-blue-600">{"Error " + id}</p>
-        <h1 className="mt-4 text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+        <h1 className="mt-4 text-2xl font-semibold tracking-tight text-gray-900">
           {errorObj[id].h1}
         </h1>
         <p className="mt-6 text-base leading-7 text-gray-600">
